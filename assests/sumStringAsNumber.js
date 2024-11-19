d5078cb0eafd42670369af18a50d07507db53012
belunatic <abelluvyou@yahoo.com>
2024-11-19 14:03:22 -0800

    had to rewrite the function as it did not pass the test

--- assests/sumStringAsNumber.js
+++ assests/sumStringAsNumber.js
@@ -4,14 +4,22 @@ Given the string representations of two integers, return the string representati
 URL: //www.codewars.com/kata/5324945e2ece5e1f32000370/train/javascript
 */
 
-https: function sumStrings(a, b) {
+function sumStrings(a, b) {
 	let result = [];
-	for (let i = 0; i < a.length; i++) {
-		result.push(Number(a[i]) + Number(b[i]));
-	}
-	return result.join("");
+	//use of BigInt data type to convert the large numbers
+	//Number data type can only handle number upto 2 ^53 -1
+	result = BigInt(a) + BigInt(b);
+	return result.toString();
 }
+
+console.log(sumStrings("123", "456"), "579");
+console.log(
+	sumStrings("712569312664357328695151392", "8100824045303269669937")
+);
 
 console.log("***Results from sumStringAsNumber.js file***");
 console.log(sumStrings("123", "456"), "579");
+console.log(
+	sumStrings("712569312664357328695151392", "8100824045303269669937")
+);
 console.log("\n");
