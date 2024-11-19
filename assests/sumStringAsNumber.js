d2fe64f1498bb0e1f8f0ee3dd2e457b267a979ec
belunatic <abelluvyou@yahoo.com>
2024-11-19 14:04:46 -0800

    remove some console.log

--- assests/sumStringAsNumber.js
+++ assests/sumStringAsNumber.js
@@ -12,11 +12,6 @@ function sumStrings(a, b) {
 	return result.toString();
 }
 
-console.log(sumStrings("123", "456"), "579");
-console.log(
-	sumStrings("712569312664357328695151392", "8100824045303269669937")
-);
-
 console.log("***Results from sumStringAsNumber.js file***");
 console.log(sumStrings("123", "456"), "579");
 console.log(
