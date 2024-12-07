5007e6270d28a86321475eef5a07ab0b5a3eb37a
belunatic <abelluvyou@yahoo.com>
2024-12-07 11:45:22 -0800

    numToString.js

---
+++ assests/numToString.js
@@ -0,0 +1,14 @@
+/*
+We need a function that can transform a number (integer) into a string.
+
+URL: https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
+*/
+function numberToString(num) {
+	return num.toString();
+}
+
+console.log("***Results numToString.js***");
+console.log(numberToString(3), '"3"');
+console.log(numberToString(-1005), '"-1005"');
+console.log(numberToString(222), '"222"');
+console.log("\n");
