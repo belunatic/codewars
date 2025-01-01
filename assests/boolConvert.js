ae6d906eea6e4b0f0a8a09e29a24f78ee6fbc2e0
belunatic <abelluvyou@yahoo.com>
2025-01-01 11:07:59 -0800

    boolCovert.js Added

---
+++ assests/boolConvert.js
@@ -0,0 +1,14 @@
+/*
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+URL: https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
+*/
+
+function boolToWord(bool) {
+	return bool ? "Yes" : "No";
+}
+
+console.log("***Results boolConvert.js***");
+console.log(boolToWord(true), "Yes");
+console.log(boolToWord(false), "No");
+console.log("\n");
